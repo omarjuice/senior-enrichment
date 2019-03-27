07d90e970d7f756402ea1a4f5df005562b4452e8
Omar Juice <ojameer1@gmail.com>
2019-03-27 11:43:16 -0400

    single views fixer upper

--- app/components/SingleStudent.jsx
+++ app/components/SingleStudent.jsx
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getSingleStudent } from '../actions';
 import { Link } from 'react-router-dom';
+import CampusCard from './CampusCard';
 
 class SingleStudent extends Component {
     componentDidMount() {
         this.props.getSingleStudent(this.props.match.params.id)
     }
     render() {
-        const { selectedStudent: { imageUrl, firstName, lastName, gpa, loading, id, email } } = this.props
+        const { selectedStudent: { imageUrl, firstName, lastName, gpa, loading, id, email, campus } } = this.props
         return (<div className="columns is-centered is-mobile is-multiline">
             <div className="column has-text-centered is-one-third-desktop is-half-tablet is-11-mobile">
                 {loading ? <i className="fas fa-circle-notch fa-spin fa-5x"></i> : <div className="card">
@@ -38,8 +39,10 @@ class SingleStudent extends Component {
 
                     </div>
                 </div>}
-
             </div>
+            {campus ? <CampusCard {...campus} /> : <div className="column is-one-third-desktop is-half-tablet is-11-mobile">
+                This student does not go to school.
+            </div>}
         </div>
         );
     }
